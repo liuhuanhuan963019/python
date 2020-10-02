189d6f6a6e7f4b99900779afdd7f224838fb4330
liuhuanhuan963019 <2432783449@qq.com>
2020-10-03 01:21:59 +0800

    模块的发布和安装
    Signed-off-by: liuhuanhuan963019 <2432783449@qq.com>

---
+++ day11-文件操作和模块/modelTest/setup.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+from distutils.core import setup
+#name  模块名称
+#version 版本号
+#description 描述
+#author  作者
+#py_modules 要发布的内容
+
+setup(name="lhh_python_demo",version="1.0",description="文件的统计模块",
+      author="liuhuanhuan",py_modules=['modeTest'])
