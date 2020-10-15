5812eddab8263c834fec21446984be3f79c130be
liuhuanhuan963019 <2432783449@qq.com>
2020-10-15 11:02:33 +0800

    数据合并
    Signed-off-by: liuhuanhuan963019 <2432783449@qq.com>

--- day17-pandas/pandas 合并concat.py
+++ day17-pandas/pandas 合并concat.py
@@ -2,30 +2,87 @@ from __future__ import print_function
 import pandas as pd
 import numpy as np
 
-# concatenating
+# concatenating  合并
 # ignore index
 df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
 df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
 df3 = pd.DataFrame(np.ones((3,4))*2, columns=['a','b','c','d'])
-res = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+# print(df1)
+# print(df2)
+# print(df3)
+'''
+     a    b    c    d
+0  0.0  0.0  0.0  0.0
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+     a    b    c    d
+0  1.0  1.0  1.0  1.0
+1  1.0  1.0  1.0  1.0
+2  1.0  1.0  1.0  1.0
+     a    b    c    d
+0  2.0  2.0  2.0  2.0
+1  2.0  2.0  2.0  2.0
+2  2.0  2.0  2.0  2.0  
+'''
+#axis  0  表示列 1表示行   ignore_index  忽略合并前的序号，重新进行排序
+# res = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+# print(res)
+'''
+    a    b    c    d
+0  0.0  0.0  0.0  0.0
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+3  1.0  1.0  1.0  1.0
+4  1.0  1.0  1.0  1.0
+5  1.0  1.0  1.0  1.0
+6  2.0  2.0  2.0  2.0
+7  2.0  2.0  2.0  2.0
+8  2.0  2.0  2.0  2.0
 
-# join, ('inner', 'outer')
+'''
+
+# join, ('inner', 'outer')  运用concat中的一个属性功能 类似与数据查询的左连接和右连接查询
 df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'], index=[1,2,3])
 df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
+print(df1)
+print(df2)
+'''
+a    b    c    d
+1  0.0  0.0  0.0  0.0
+2  0.0  0.0  0.0  0.0
+3  0.0  0.0  0.0  0.0
+     b    c    d    e
+2  1.0  1.0  1.0  1.0
+3  1.0  1.0  1.0  1.0
+4  1.0  1.0  1.0  1.0
+'''
 res = pd.concat([df1, df2], axis=1, join='outer')
-res = pd.concat([df1, df2], axis=1, join='inner')
-
+'''
+    a    b    c    d    b    c    d    e
+1  0.0  0.0  0.0  0.0  NaN  NaN  NaN  NaN
+2  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+3  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+4  NaN  NaN  NaN  NaN  1.0  1.0  1.0  1.0 
+'''
+#默认的删除那些已为空的或者不匹配的数据
+res = pd.concat([df1, df2], axis=1, join='inner',ignore_index=True)
+'''
+    0    1    2    3    4    5    6    7
+2  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+3  0.0  0.0  0.0  0.0  1.0  1.0  1.0  1.0
+'''
+#
 # join_axes
 res = pd.concat([df1, df2], axis=1, join_axes=[df1.index])
-
-# append
+#
+# # append
 df1 = pd.DataFrame(np.ones((3,4))*0, columns=['a','b','c','d'])
 df2 = pd.DataFrame(np.ones((3,4))*1, columns=['a','b','c','d'])
 df2 = pd.DataFrame(np.ones((3,4))*1, columns=['b','c','d', 'e'], index=[2,3,4])
 res = df1.append(df2, ignore_index=True)
 res = df1.append([df2, df3])
-
+#
 s1 = pd.Series([1,2,3,4], index=['a','b','c','d'])
 res = df1.append(s1, ignore_index=True)
-
+#
 print(res)
--- day17-pandas/pandas 导入导出.py
+++ day17-pandas/pandas 导入导出.py
@@ -1,13 +1,9 @@
-from __future__ import print_function
 import pandas as pd
-import numpy as np
-
-dates = pd.date_range('20130101', periods=6)
-df = pd.DataFrame(np.arange(24).reshape((6,4)), index=dates, columns=['A', 'B', 'C', 'D'])
 
 
-df.iloc[0,1] = np.nan
-df.iloc[1,2] = np.nan
-print(df.dropna(axis=0, how='any'))   # how={'any', 'all'}
-print(df.fillna(value=0))
-print(pd.isnull(df))
+#read_csv  读取csv格式的数据
+#read_excel  读取excel格式的数据
+data = pd.read_excel('demo.xlsx')
+print(data)
+
+
